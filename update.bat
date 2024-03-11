cf20305f2eee4d61029f9567187d6380e3ac1a70
TU. CHENG SHIN <tutumomo@gmail.com>
2024-03-11 21:44:04 +0800

    U

--- update.bat
+++ update.bat
@@ -8,12 +8,6 @@ git pull
 call ac
 pip install -U ollama orjson gunicorn litellm streamlit gradio litellm[proxy]
 
-if not exist ollama-libraries-example (
-   git clone https://github.com/tutumomo/ollama-libraries-example.git
-) else (
-   cd ollama-libraries-example
-   git pull
-   cd ..
-)
+call sub_update ollama-libraries-example
 
 pause
